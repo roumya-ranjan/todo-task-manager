9a6217fdf8744833434881942bf6cf6dbe50dfcb
Roumya <roumyaranjan2@gmail.com>
2026-05-29 16:24:26 +0530

    Added User and Todo entity classes

---
+++ src/main/java/com/todo/app/entiry/Todo.java
@@ -0,0 +1,69 @@
+package com.todo.app.entiry;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="todos")
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	private String title;
+	private String description;
+	private String status;
+	private LocalDate dueDate;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", title=" + title + ", description=" + description + ", status=" + status
+				+ ", dueDate=" + dueDate + ", user=" + user + "]";
+	}
+
+}
